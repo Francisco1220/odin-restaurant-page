0b0ca20133e8d3435322ba7eff61eaacbc27075e
Francisco <franciscocolombocouste@gmail.com>
2024-09-24 21:46:58 +0200

    Edit innerHTML

--- src/home.js
+++ src/home.js
@@ -17,7 +17,8 @@ export function homePage () {
     const divContent = document.querySelector("#content");
     divContent.innerHTML = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac commodo sapien. Integer rutrum pretium enim sed tempus. In vitae dapibus enim, ac sodales felis. 
     Nullam laoreet urna sit amet gravida fermentum. Integer eleifend arcu tempor rhoncus iaculis. Donec pulvinar enim sit amet orci porttitor, eu condimentum nisi dignissim. 
-    Duis quam sem, euismod a commodo sed, molestie quis nunc.`;
+    Duis quam sem, euismod a commodo sed, molestie quis nunc. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae 
+    ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.`;
     parentContainer.insertBefore(childContainer, divContent);
 
     // Create .overlayText div, set class, and and append it to the second container. Add text to .overlayText. Use CSS styling to overlay
