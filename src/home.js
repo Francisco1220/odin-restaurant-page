f0f52a2f5b63fd1713a0e4aa2306e3b959d0a538
Francisco <franciscocolombocouste@gmail.com>
2024-09-25 16:42:09 +0200

    Change DOM element scope

--- src/home.js
+++ src/home.js
@@ -1,28 +1,29 @@
 // Import homepage image
 import homePageImage from "./assets/images/homepage.jpg";
 
+// Create content div, child container div, image div, and overlay text div
+export const divContent = document.querySelector("#content");
+export const childContainer = document.createElement("div");
+export const image = document.createElement("img");
+export const overlayTxtDiv = document.createElement("div");
+
 export function homePage () {
-    // Create a child container div, give it a class, and append to parent container div
-    const childContainer = document.createElement("div");
+    // Give child container div a class, and append to parent container div
     childContainer.setAttribute("class", "child-container");
     const parentContainer = document.querySelector(".parent-container");
-
-    // Create image div and append it to child container div
-    const image = document.createElement("img");
+    // Append image div to child container div
     image.setAttribute("id", "home-image");
     image.src = homePageImage;
     childContainer.appendChild(image);
 
     // Grab div#content. Add text to div#content, and append it to child container
-    const divContent = document.querySelector("#content");
     divContent.innerHTML = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac commodo sapien. Integer rutrum pretium enim sed tempus. In vitae dapibus enim, ac sodales felis. 
     Nullam laoreet urna sit amet gravida fermentum. Integer eleifend arcu tempor rhoncus iaculis. Donec pulvinar enim sit amet orci porttitor, eu condimentum nisi dignissim. 
     Duis quam sem, euismod a commodo sed, molestie quis nunc. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae 
     ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.`;
     parentContainer.insertBefore(childContainer, divContent);
 
-    // Create .overlayText div, set class, and and append it to the second container. Add text to .overlayText. Use CSS styling to overlay
-    const overlayTxtDiv = document.createElement("div");
+    // Set class for overlayTxtDiv, and and append it to the second container. Add text to .overlayText
     overlayTxtDiv.setAttribute("class","overlay-text");
     overlayTxtDiv.innerHTML = "Authentic Indian Food";
     childContainer.appendChild(overlayTxtDiv);
