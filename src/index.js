3b4d72568d332cf565f323c23fa8f7acae9330bf
Francisco <franciscocolombocouste@gmail.com>
2024-09-25 16:43:44 +0200

    Add webpage tabs functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import {homePage} from "./home.js";
+import {homePage, divContent, childContainer, image, overlayTxtDiv} from "./home.js";
 import {menuPage} from "./menu.js";
+import {aboutPage} from "./about.js";
 
 // Select buttons from the DOM
 const homeBtn = document.querySelector("button:first-child");
@@ -7,7 +8,35 @@ const menuBtn = document.querySelector("button:nth-child(2)");
 const aboutBtn = document.querySelector("button:nth-child(3)");
 
 // Handle code execution from home.js, menu.js, and about.js by adding event listeners
-homePage();
+
+let clickCounter = 0;
+if (clickCounter === 0) {
+    homePage();
+    clickCounter++;
+}
+
+homeBtn.addEventListener("click", () => {
+    childContainer.remove();
+    image.remove();
+    overlayTxtDiv.remove();
+    // Remove contents of div#content
+    divContent.innerHTML = "";
+    homePage();
+})
+
+menuBtn.addEventListener("click", () => {
+    // Remove childContainer
+    console.log(childContainer);
+    childContainer.remove();
+    menuPage();
+})
+
+aboutBtn.addEventListener("click", () => {
+    // Remove contents of div#content
+    console.log(childContainer);
+    childContainer.remove();
+    aboutPage();
+})
 
 
 // Import css styles
