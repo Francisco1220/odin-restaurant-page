818129638c0d8c715cbeb4ccd421e34f2cba39ad
Francisco <franciscocolombocouste@gmail.com>
2024-09-25 10:52:12 +0200

    Import homepage

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import {homePage} from "./home.js";
+
+// Select buttons from the DOM
+
+// Handle code execution from home.js, menu.js, and about.js by adding event listeners
+homePage();
+
+// menuBtn.addEventListener("click", menuPage);
+
+// Import css styles
+
+
+
