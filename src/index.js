00c103b4f88482a9d67a71fe5ceb47d6a6ff80a6
Francisco <franciscocolombocouste@gmail.com>
2024-09-25 11:16:47 +0200

    Add button selectors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import {homePage} from "./home.js";
 
+
 // Select buttons from the DOM
+const homeBtn = document.querySelector("button:first-child");
+const menuBtn = document.querySelector("button:nth-child(2)");
+const aboutBtn = document.querySelector("button:nth-child(3)");
 
 // Handle code execution from home.js, menu.js, and about.js by adding event listeners
 homePage();
-
-// menuBtn.addEventListener("click", menuPage);
 
 
 // Import css styles
